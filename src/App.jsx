c87b44c56a9c0ffb0efefab523fa2009cfd927c3
mujkanovic01 <bakir.mujkanovic01@gmail.com>
2021-03-20 16:40:23 +0100

    feature: login and signup components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpView from "./views/SignUpView";
+import SignInView from "./views/SignInView";
+
+const App = () => {
+  return (
+    <div>
+      <SignUpView></SignUpView>
+      <SignInView />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/views/SignUpView.jsx
@@ -0,0 +1,131 @@
+import React, { useRef } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Brocode
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const nameInput = useRef();
+  const classes = useStyles();
+
+  const handleSubmit = () => {
+    const name = nameInput.current[0].value;
+    const email = nameInput.current[2].value;
+    const password = nameInput.current[4].value;
+
+    //todo: post to backend
+    console.log(name, email, password);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Registracija
+        </Typography>
+        <form className={classes.form} noValidate ref={nameInput}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="name"
+                variant="outlined"
+                required
+                fullWidth
+                id="name"
+                label="Ime i prezime"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email adresa"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Registracija
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              {/** Add routing to sign in page */}
+              <Link href="#" variant="body2">
+                Već ste registrovani? Prijavite se
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
