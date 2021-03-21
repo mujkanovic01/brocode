869c40be3fc5572106448939e973e9f4a8ee09d3
tanovicazur <azur.tanovic.2016@gmail.com>
2021-03-21 04:51:10 +0100

    feat: questions class

--- src/api/api.js
+++ src/api/api.js
@@ -44,6 +44,111 @@ class api {
         localStorage.setItem("Authorization", `Bearer ${data.accessToken}`);
       });
   }
+
+  getAllClasses(){
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`${url.url}/classes`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  getClassesById(id){
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("id", id);
+
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`${url.url}/classes/byId/${id}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  getQuestionsByClassId(class_id){
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("class_id", class_id);
+
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`${url.url}/questions/byClassId/${class_id}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  postQuestion(desc, isanonymous, user_id, class_id, title){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("desc", desc);
+    urlencoded.append("isanonymous", isanonymous);
+    urlencoded.append("user_id", user_id);
+    urlencoded.append("class_id", class_id);
+    urlencoded.append("title", title);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch(`${url.url}/questions/newQuestion`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  getAnswersByQuestionId(question_id){
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("question_id", question_id);
+
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`${url.url}/answers/byQuestionId/${question_id}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  /*postAnswer(){                              WIP
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("desc", desc);
+    urlencoded.append("isanonymous", isanonymous);
+    urlencoded.append("user_id", user_id);
+    urlencoded.append("class_id", class_id);
+    urlencoded.append("title", title);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch(`${url.url}/questions/newQuestion`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }*/
 }
 
 export default new api();
