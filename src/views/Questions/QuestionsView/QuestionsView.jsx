2ae813569bba7db336a7cafffad3a49c1eef252f
mujkanovic01 <bakir.mujkanovic01@gmail.com>
2021-03-21 02:42:56 +0100

    feature: navbar & sidebar

--- src/views/Questions/QuestionsView/QuestionsView.jsx
+++ src/views/Questions/QuestionsView/QuestionsView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import NavigationBar from "../../../components/organisms/NavigationBar/NavigationBar";
 import "./QuestionsView.scss";
 
@@ -6,7 +7,34 @@ const QuestionsView = () => {
   return (
     <div className="QuestionsView">
       <NavigationBar></NavigationBar>
-      <div className="QuestionView__SideBar"></div>
+      <div className="QuestionsView__SideBar">
+        <div className="QuestionsView__SideBar__Title">
+          <p>Predmeti</p>
+        </div>
+        <div className="QuestionsView__SideBar__Classes">
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+          <Link className="QuestionsView__SideBar__Classes__Class">
+            Matematika
+          </Link>
+        </div>
+      </div>
+      <div className="QuestionsView__MainView">
+        <p>aaaaaaaaaaaaaaa</p>
+      </div>
     </div>
   );
 };
